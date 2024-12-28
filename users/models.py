b224c6ad4526fbf7481310052dfb05b1c91f0b5b
Server <server@gmail.com>
2024-12-28 12:38:41 +0000

    .

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from django.db import models
 
 # Create your models here.
@@ -12,4 +13,20 @@ class User(AbstractUser):
     profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
 
     def __str__(self):
+=======
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    firstName = models.CharField(max_length=32)
+    lastName = models.CharField(max_length=32)
+    username = models.CharField(max_length=20, unique=True)
+    email = models.CharField(max_length=512)
+    password = models.CharField(max_length=32)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
+    def __str__(self):
+>>>>>>> Stashed changes
         return self.username
